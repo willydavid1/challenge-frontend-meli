abdd6dd13f56badf9aa03e6e27a8359bf73eedf5
Willy Da Conceicao <willydavidlozada@gmail.com>
2021-03-03 07:02:09 -0500

    🎉 Base project

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
-import '../styles/globals.css'
+import React from 'react'
+import 'styles/globals.sass'
 
-function MyApp({ Component, pageProps }) {
+function MyApp ({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
