84744ced7f06719d3897036613da9a12828ebc44
Willy Da Conceicao <willydavidlozada@gmail.com>
2021-03-04 22:24:33 -0500

    🔍 Improving seo a bit with the head tag

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import { Header } from 'components/header'
+import Head from 'next/head'
 
 const Layout = ({ children }) => {
   return (
     <div>
+      <Head>
+        <title>Buscador de Mercadolibre</title>
+        <meta name="description" content="En esta app puedes buscar cualquier producto disponible en mercadolibre de forma rápida"/>
+        <meta name="keywords" content="buscador de productos mercadolibre, buscador de productos, información mercadolibre"/>
+      </Head>
       <Header />
       <div className="container">
         {children}
